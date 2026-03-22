d2be2e58f852439cacd149bcda4aee6c1242a7b6
Ribka Mafito <rebeccamafitomamo@gmail.com>
2026-03-22 21:21:21 +0300

    use the datat form the back end

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,12 +1,15 @@
 import axios from 'axios'
+import { useEffect, useState } from 'react';
 import Header  from "../Componet/Header";
 import "./HomePage.css";
 import {products} from "../../Starting-code/data/products"
 function HomePage() {
-  axios.get('http://localhost:3000/api/products')
+  const [products, setProducts] = useState([])
+  useEffect(() =>
+ { axios.get('http://localhost:3000/api/products')
   .then((response) =>{
-    console.log(response.data)
-  })
+    setProducts(response.data)
+  })},[])
   return (
     <>
       <title>Ecommerce Project</title>
