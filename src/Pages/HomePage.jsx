bf9623ba814fbf3d331608df5d68497c443acfda
Ribka Mafito <rebeccamafitomamo@gmail.com>
2026-03-22 20:52:27 +0300

    fetch the data from backend

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -2,6 +2,13 @@ import Header  from "../Componet/Header";
 import "./HomePage.css";
 import {products} from "../../Starting-code/data/products"
 function HomePage() {
+  fetch('http://localhost:3000/api/products')
+  .then((response) =>{
+    response.json()
+
+  }).then((data) =>{
+      console.log(data)
+    })
   return (
     <>
       <title>Ecommerce Project</title>
