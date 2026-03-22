d61e601b6a19b95c595843d589b5374217c1c656
Ribka Mafito <rebeccamafitomamo@gmail.com>
2026-03-22 16:01:09 +0300

    check out split and link

--- src/App.jsx
+++ src/App.jsx
@@ -10,9 +10,9 @@ import Favorites from "./page/Favorites";
 import Navbar from "./componets/Navbar";*/
 //import DigitalClock from "./DigitalClock";
 //import Note from "./Note";
-import { Routes,Route } from "react-router-dom"; 
+import { Routes, Route } from "react-router-dom";
 import HomePage from "./Pages/HomePage";
-import CheckoutPage from "./Pages/CheckoutPage";
+import CheckoutPage from "./Pages/Checkout/CheckoutPage";
 import Orders from "./Pages/OrdersPage";
 import Tracking from "./Pages/TrackingPage";
 function App() {
@@ -28,16 +28,15 @@ function App() {
       </main>
     </MovieProvider>
   );*/
-  return(
+  return (
     <Routes>
-      <Route index element={<HomePage/>}/>
-      <Route path="/checkout" element={<CheckoutPage/>}/>
-      <Route path="/orders" element={<Orders/>}/>
-      <Route path="/tracking" element={<Tracking/>}/>
+      <Route index element={<HomePage />} />
+      <Route path="/checkout" element={<CheckoutPage />} />
+      <Route path="/orders" element={<Orders />} />
+      <Route path="/tracking" element={<Tracking />} />
     </Routes>
     //
-
-  )
+  );
 }
 
 export default App;
